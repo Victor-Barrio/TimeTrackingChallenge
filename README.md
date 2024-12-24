00ab0029151e338f7d66076728a4a74332642f9d
Victor <victorbarrio82@gmail.com>
2024-12-24 17:47:23 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![](./design/desktop-design.jpg)
+
+### Links
+
+- Solution URL: [https://github.com/Victor-Barrio/TimeTrackingChallenge]
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+### What I learned
+
+In this project i worked with the JQuery library and AJAX request to get data from a JSON file. I also used the CSS custom properties to create a responsive design. 
---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Creating the variables for the elements
+const timeDaily = document.getElementById("timeDaily");
+const timeWeekly = document.getElementById("timeWeekly");
+const timeMonthly = document.getElementById("timeMonthly");
+
+let actualHrs = document.querySelectorAll(".actualHours");
+let previousHrs = document.querySelectorAll(".previousHours");
+
+// Starting the function
+const updateHours = (timeFrame, activeTimeElement) => {
+  // Changing the class of the time options
+  timeDaily.classList.remove("active");
+  timeWeekly.classList.remove("active");
+  timeMonthly.classList.remove("active");
+  activeTimeElement.classList.add("active");
+
+  // Adding the info of the hours with Ajax
+  $.ajax({
+    url: "../data.json",
+    type: "GET",
+    success: function (data) {
+      // Working with every card of the DOM
+      actualHrs.forEach((actual, index) => {
+        const previous = previousHrs[index]; // Getting the element
+        const activityData = data[index]; // Getting the corresponent data from the JSON
+
+        // Actualizing the hours
+        actual.textContent = `${activityData.timeframes[timeFrame].current}hrs`;
+        previous.textContent = `Last ${timeFrame} - ${activityData.timeframes[timeFrame].previous}hrs`;
+      });
+    },
+    error: function (xhr, status) {
+      console.log(`Error on load ${xhr} in the ${status}`);
+    },
+  });
+};
+
+// Adding the event listeners
+timeDaily.addEventListener("click", () => updateHours("day", timeDaily));
+timeWeekly.addEventListener("click", () => updateHours("week", timeWeekly));
+timeMonthly.addEventListener("click", () => updateHours("month", timeMonthly));
